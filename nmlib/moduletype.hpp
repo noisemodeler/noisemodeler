938a6731e974b6ab2c16b6caa1a0fe69cb54a038
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2014-01-22 13:51:53 +0100

    Stubs for ModuleType

--- nmlib/moduletype.hpp
+++ nmlib/moduletype.hpp
@@ -1,12 +1,25 @@
 #ifndef NM_MODULETYPE_HPP
 #define NM_MODULETYPE_HPP
 
+#include <vector>
+
 namespace nm {
 
+class ModuleOutput;
+class ModuleInput;
+
+/**
+ * @brief Describes a module and its inputs and outputs.
+ */
 class ModuleType
 {
 public:
     ModuleType();
+    const ModuleInput* getInput(std::string name) const;
+    const ModuleOutput* getOutput(std::string name) const;
+    std::vector<ModuleOutput*> outputs() const;
+    std::vector<ModuleInput*> inputs() const;
+
 };
 
 } // namespace nm
