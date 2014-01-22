c6ac9a67ef269426e2549a4d62299061671fddfb
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2014-01-22 14:05:16 +0100

    More doxygen comments

--- nmlib/signaltype.hpp
+++ nmlib/signaltype.hpp
@@ -3,12 +3,23 @@
 
 namespace nm {
 
+/**
+ * @brief Describes a the dimensionality of a signal
+ *
+ * Can be extended to include other types such as distinctions between doubles, flots and ints as well.
+ */
 class SignalType
 {
 public:
     const int dimensionality;
     SignalType(int dimensionality);
     bool operator==(const SignalType& rhs) const;
+
+    /**
+     * @brief Checks if this SignalType can be converted to another.
+     * @param SignalType to convert to
+     * @return
+     */
     bool isConvertibleTo(const SignalType &rhs) const;
 };
 
