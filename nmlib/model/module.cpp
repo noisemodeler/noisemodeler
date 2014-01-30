af3cd4a958ac74266ead9e6f5212811c91c5ab96
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2014-01-30 22:03:48 +0100

    Module constructor

--- nmlib/model/module.cpp
+++ nmlib/model/module.cpp
@@ -2,6 +2,7 @@
 
 #include <nmlib/model/moduleinput.hpp>
 #include <nmlib/model/moduleoutput.hpp>
+#include <nmlib/model/moduletype.hpp>
 
 #include <algorithm>
 
@@ -11,8 +12,12 @@ Module::Module(const ModuleType &type, std::string name):
     c_type(type),
     c_name(name)
 {
-    //loop through moduleinputs and add inputlinks
-    //and outputs
+    for(auto &moduleInput : c_type.inputs()){
+        m_inputs.push_back(InputLink(*this, *moduleInput));
+    }
+    for(auto &moduleOutput : c_type.outputs()){
+        m_outputs.push_back(OutputLink(*this, *moduleOutput));
+    }
 }
 
 InputLink *Module::getInput(std::string name)
