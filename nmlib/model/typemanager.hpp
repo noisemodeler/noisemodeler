75ad93467a58eee6d25049fa15a9ee398412ad88
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2014-02-16 15:56:24 +0100

    Draft for system to add bultintypes

--- nmlib/model/typemanager.hpp
+++ nmlib/model/typemanager.hpp
@@ -20,11 +20,16 @@ public:
     bool addUserType(std::unique_ptr<CompositeModuleType> type);
     const ModuleType *getType(std::string name) const;
     CompositeModuleType *getUserType(std::string name);
+    enum class BuiltinType {
+        ADD1, SUB1, MUL1,
+        DEMUX2 //and more...
+    };
     void initBuiltinTypes();
+
 private:
     void addBuiltinType(std::unique_ptr<const PrimitiveModuleType> moduleType);
     std::map<std::string, std::unique_ptr<CompositeModuleType>> m_userTypes;
-    std::map<std::string, std::unique_ptr<const PrimitiveModuleType>> m_primitiveBuiltinTypes;
+    std::map<std::string, std::unique_ptr<const ModuleType>> m_primitiveBuiltinTypes;
 //    std::map<std::string, std::unique_ptr<CompositeModuleType>> m_compositeBuiltinTypes;
     static const PrimitiveModuleType
     //1D only types
