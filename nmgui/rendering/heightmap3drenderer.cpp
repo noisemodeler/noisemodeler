1a5d7c46b8c7c88a3a2e1b414fca59848be690f9
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2014-03-14 11:28:38 +0100

    In the middle of implementing cdlod

--- nmgui/rendering/heightmap3drenderer.cpp
+++ nmgui/rendering/heightmap3drenderer.cpp
@@ -8,6 +8,68 @@
 #include <iostream>
 #include <QDebug>
 
+namespace {
+
+bool lineSegmentIntersectsSphere(QVector3D p1, QVector3D p2, QVector3D sphereCenter, float radius){
+    // formula from: http://paulbourke.net/geometry/circlesphere/index.html
+    QVector3D &p3 = sphereCenter;
+    QVector3D p1p2 = p2-p1;
+    const float numerator = QVector3D::dotProduct((p3-p1)*(p1p2),{1,1,1});
+    const float denominator = (p1p2).lengthSquared();
+    const float u = numerator / denominator;
+    if(u<0 || u>1)return false; //line intersection outside line segment
+    QVector3D p = p1 + p1p2*u; //intersection point
+    QVector3D p3p = p - p3; //vector from center to intersection point
+    const float rsquared = radius * radius;
+    return p3p.lengthSquared() < rsquared; //checking if closest point on segment is closer to the center than the radius
+}
+
+struct TerrainPatchSelection {
+    int lodLevel;
+    QVector3D position;//lower left corner
+    float size;
+    void visitChildren(std::function<void(TerrainPatchSelection)> cb){
+        cb(TerrainPatchSelection{lodLevel-1, position, size/2});
+        cb(TerrainPatchSelection{lodLevel-1, position+QVector3D{1,0,0}*size/2, size/2});
+        cb(TerrainPatchSelection{lodLevel-1, position+QVector3D{0,0,-1}*size/2, size/2});
+        cb(TerrainPatchSelection{lodLevel-1, position+QVector3D{1,0,-1}*size/2, size/2});
+    }
+    /** @brief appends appropriate terrainpatches to a list of selections
+      * @param ranges the radiuses of different lod ranges
+      * @param observerPosition
+      * @param selections the collection of selections to draw
+      * @return whether the area was covered by this patch or its children
+      */
+    bool lodSelect(const std::vector<float> &ranges, const QVector3D &observerPosition, std::vector<TerrainPatchSelection> &selections){
+        //if this is final lodlevel, or sphere do not intersect, then this is the final lodlevel for this branch of the quad tree
+        if(lodLevel==0 || !patchIntersectsSphere(ranges[lodLevel-1], observerPosition)){
+            selections.push_back(*this);
+            return true;
+        }
+        visitChildren([&](TerrainPatchSelection patch){
+            patch.lodSelect(ranges, observerPosition, selections);
+        });
+        return true;
+    }
+    bool patchIntersectsSphere(float radius, QVector3D sphereCenter){
+        QVector3D bl = position;
+        QVector3D br = bl + QVector3D{1,0, 0}*size; //TODO ideally get height from terrain data here
+        QVector3D tl = bl + QVector3D{0,0,-1}*size;
+        QVector3D tr = bl + QVector3D{1,0,-1}*size;
+        //first check if center is inside
+        if(sphereCenter.x()>bl.x() && sphereCenter.x()<br.x() &&
+           sphereCenter.z()>bl.z() && sphereCenter.z()<tr.z()){
+            return true;
+        }
+        return lineSegmentIntersectsSphere(bl,br,sphereCenter,radius) ||
+               lineSegmentIntersectsSphere(bl,tl,sphereCenter,radius) ||
+               lineSegmentIntersectsSphere(br,tr,sphereCenter,radius) ||
+               lineSegmentIntersectsSphere(tl,tr,sphereCenter,radius);
+    }
+};
+
+}
+
 namespace nmgui {
 
 HeightMap3DRenderer::HeightMap3DRenderer():
@@ -33,6 +95,14 @@ void HeightMap3DRenderer::setState(HeightMap3DExplorer::State &state)
 }
 
 void HeightMap3DRenderer::render(){
+    //select terrain patches of different lod levels here
+    //TODO maybe this is not the right place for this kind of code...
+    std::vector<TerrainPatchSelection> selections;
+    TerrainPatchSelection rootQuad{0,{0,0,0},100};
+    rootQuad.lodSelect({10,8,4,2},m_state.camera.position(),selections);
+
+
+
     glClearColor(0.5, 0.7, 1, 1);
     glDepthMask(GL_TRUE);
     glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
@@ -51,8 +121,6 @@ void HeightMap3DRenderer::render(){
     QVector4D domain{l, t, w, h};
     m_program->setUniformValue("domain", domain);
 
-    QMatrix4x4 modelMatrix;
-    modelMatrix.scale(300);
 
     //get viewmatrix from camera
     QMatrix4x4 viewMatrix = m_state.camera.worldToLocalMatrix();
@@ -64,7 +132,9 @@ void HeightMap3DRenderer::render(){
 //    projectionMatrix.ortho(-10,10,-10,10,0.10,10);
     projectionMatrix.scale({1,-1,1}); //flip Y coordinates because otherwise Qt will render it upside down
 
-    //precompute model view projection matrix
+    QMatrix4x4 modelMatrix;
+    modelMatrix.scale(100);
+    modelMatrix.translate(rootQuad.position);
     QMatrix4x4 mvp = projectionMatrix * modelViewMatrix;
 
     //Pass matrices to shader
@@ -81,7 +151,9 @@ void HeightMap3DRenderer::render(){
     //this is where the magic happens
     {
         QOpenGLVertexArrayObject::Binder binder(&m_vao);
-        glDrawArrays(GL_TRIANGLE_STRIP, 0, m_vertexCount);
+        for (int i = 0; i < 1; ++i) {
+            glDrawArrays(GL_LINE_STRIP, 0, m_vertexCount);
+        }
     }
 }
 
