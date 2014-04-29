29d037fc0375b96e2a2e191e248a85e94b19f36c
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2014-04-30 01:14:58 +0200

    Comment out unused parameters

--- nmlib/codegeneration/compositemodulegenerator.cpp
+++ nmlib/codegeneration/compositemodulegenerator.cpp
@@ -6,12 +6,12 @@ CompositeModuleGenerator::CompositeModuleGenerator()
 {
 }
 
-void CompositeModuleGenerator::generateDefaults(InlineGenerator &gen, std::ostream &out)
+void CompositeModuleGenerator::generateDefaults(InlineGenerator &/*gen*/, std::ostream &/*out*/)
 {
     //just use unlinkeddefaults
 }
 
-void CompositeModuleGenerator::generateBody(InlineGenerator &gen, std::ostream &out)
+void CompositeModuleGenerator::generateBody(InlineGenerator &/*gen*/, std::ostream &/*out*/)
 {
     //gather inputs/outputs
     //create remaps
