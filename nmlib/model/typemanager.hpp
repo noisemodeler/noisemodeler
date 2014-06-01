c908f1cf5c790c40001c0b326d0a9e7a46b105f5
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2014-06-01 17:12:46 +0200

    Doxygen for TypeManager

--- nmlib/model/typemanager.hpp
+++ nmlib/model/typemanager.hpp
@@ -13,20 +13,63 @@ class PrimitiveModuleType;
 class CompositeTypeBuilder;
 
 /**
- * @brief The top-level entity of a noise model. Encapsulates several user types as well as built-in types.
  * @ingroup model
+ * @brief The top-level entity of a noise model. Encapsulates several
+ * user types as well as built-in types.
+ *
+ * A type manager is the top-level entity in the model hierarchy.
+ *
+ * Two lists of ModuleType%s are maintained in a TypeManager, the user types,
+ * and the built-in types.
+ *
+ * A user type may be constructed explicitly, or may be returned from a Parser
+ * in the form of a unique_ptr.
+ *
+ * New user types can conveniently be added to the type manager, using the method
+ * createUserType().
+ * 
+ * A TypeManager owns the ModuleType%s it manages.
+ *
  */
 class TypeManager : public UserDataProvider
 {
 public:
+    /**
+     * @brief Creates a new TypeManager
+     *
+     * Note that initBuiltinTypes method has to be called explicitly to
+     * populate the list of buil-in types.
+     */
     TypeManager();
     ~TypeManager();
+    /**
+     * @brief Add an existing user type to the TypeManager.
+     * @return true if there was not a naming conflict and the type was added.
+     *
+     * To create a new user type, consider using createUserType instead.
+     */
     bool addUserType(std::unique_ptr<ModuleType> moduleType);
+    /** 
+     * @brief try to create a new usertype with the given name
+     * @return If a ModuleType was created, a pointer to it is returned.
+     * Note that the ModuleType is still owned by the TypeManager.
+     *
+     * If the name is unavailable a different name may be chosen.
+     */
     ModuleType* createUserType(std::string desiredName);
+    /**
+     * @brief Search for a module type with the given name
+     * @return A pointer to a user type or built-in type, or nullptr
+     * if it was not found.
+     */
     const ModuleType *getType(std::string name) const;
+    /** @brief Search for a built-in type with the given name */
     const ModuleType *getBuiltinType(std::string name) const;
+    /** @brief Search for a user type with the given name */
     const ModuleType *getUserType(std::string name) const;
-    ModuleType *getUserType(std::string name); //this is used to be able to edit usertypes
+    /** @brief Search for a user type with the given name */
+    ModuleType *getUserType(std::string name);
+    /** @brief Populate the list of built-in module types */
     void initBuiltinTypes();
 
     //index accessors
@@ -36,7 +79,9 @@ public:
     const ModuleType *getUserType(unsigned int index) const { return m_userTypes.at(index).get(); }
 
     //signals
+    /** @brief This signal is emitted before the TypeManager is destroyed */
     signal<void(TypeManager&)> destroying;
+    /** @brief This signal is emitted when the list of user type changes */
     signal<void(TypeManager&)> userTypesChanged;
 
 private:
