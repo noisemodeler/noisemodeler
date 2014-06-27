62e6c184c89d3908396a0f20cfbc1abd0f82529e
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2014-06-27 12:53:35 +0200

    Test for code generation

--- test_nmlib/test_codegeneration_glsl.cpp
+++ test_nmlib/test_codegeneration_glsl.cpp
@@ -4,5 +4,25 @@
 
 namespace {
 TEST(GLSLCodeGenerationTest, compileWithoutErrors){
+    //create a simple fbm terrain
+    nm::TypeManager typeManager;
+    typeManager.initBuiltinTypes();
+    nm::ModuleType* terrainType = typeManager.createUserType("terrain");
+    terrainType->addInput("pos", nm::SignalType{2});
+    terrainType->addOutput("height", nm::SignalType{1});
+    const nm::ModuleType *fbmType = typeManager.getType("fbm2");
+    ASSERT_NE(nullptr, fbmType);
+    nm::Module* fbmModule = terrainType->getGraph()->createModule(*fbmType);
+    fbmModule->getInput("pos")->link(*terrainType->getInputModule()->getOutput("pos"));
+    fbmModule->getOutput("result")->addLink(*terrainType->getOutputModule()->getInput("height"));
+
+    //TODO, create a simpler function
+    std::string glslSource = nm::glsl::GlslGenerator::compileToGlslFunction(
+                *terrainType->getInputModule()->getInput("pos"),
+                *terrainType->getOutputModule()->getOutput("height"),
+                "terrainFunction");
+
+    EXPECT_NE("", glslSource);
+    //TODO more tests for generated source code
 }
 }
