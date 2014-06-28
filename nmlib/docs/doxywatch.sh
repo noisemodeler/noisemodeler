3eec105f7cf719210f4b492735dcfa631a643305
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2014-06-28 17:25:02 +0200

    Moved doxygen related files into a docs subdirectory.

--- nmlib/docs/doxywatch.sh
+++ nmlib/docs/doxywatch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 while true; do
-  change=$(inotifywait -r -e close_write,moved_to,create,move .)
+  change=$(inotifywait -r -e close_write,moved_to,create,move ..)
   changeFilename=${change#./ * }
   fileExtension=${changeFilename##*.}
   if [ "$fileExtension" = "hpp" ]; then 
