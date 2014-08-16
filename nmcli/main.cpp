902e51e0df548510c5f76d74584eb3f71346b8c1
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2014-08-16 21:31:05 +0200

    Cleaned up formatting.
    Replaced tabs with spaces

--- nmcli/main.cpp
+++ nmcli/main.cpp
@@ -18,49 +18,49 @@ nm::optional<std::string> readFile(std::string path)
 }
 
 int main(int argc, char *argv[]){
-	//handle arguments
-	if(argc!=2){
-		std::cout << "Example usage:\n\tnm terrain.nm.json\n";
-		std::cout << "\nThis will generate a GLSL shader for the terrain function in the file \"terrain.nm.json\"\n";
-		return EXIT_SUCCESS;
-	} 
-	std::string filepath = argv[1];
-	std::cout << "Generating code for terrain file: " << filepath << "\n";
+    //handle arguments
+    if(argc!=2){
+        std::cout << "Example usage:\n\tnm terrain.nm.json\n";
+        std::cout << "\nThis will generate a GLSL shader for the terrain function in the file \"terrain.nm.json\"\n";
+        return EXIT_SUCCESS;
+    }
+    std::string filepath = argv[1];
+    std::cout << "Generating code for terrain file: " << filepath << "\n";
 
-	//read file
-	auto maybeFile = readFile(filepath);
-	if(!maybeFile){
-		std::cerr << "Error: Couldn't open file: " << filepath << "\n";
-		return EXIT_FAILURE;
-	}
+    //read file
+    auto maybeFile = readFile(filepath);
+    if(!maybeFile){
+        std::cerr << "Error: Couldn't open file: " << filepath << "\n";
+        return EXIT_FAILURE;
+    }
 
-	//parse document
-	std::string json = *maybeFile;
-	nm::Parser parser;
-	auto maybeTypeManager = parser.parseDocument(json);
-	if(!maybeTypeManager){
-		std::cerr << "Error: Couldn't parse document: " << filepath << "\n";
-		return EXIT_FAILURE;
-	}
+    //parse document
+    std::string json = *maybeFile;
+    nm::Parser parser;
+    auto maybeTypeManager = parser.parseDocument(json);
+    if(!maybeTypeManager){
+        std::cerr << "Error: Couldn't parse document: " << filepath << "\n";
+        return EXIT_FAILURE;
+    }
 
-	//get the first module type
-	nm::TypeManager& typeManager =  **maybeTypeManager;
-	unsigned int first = 0;
-	auto moduleType = typeManager.getUserType(first);
-	if(moduleType==nullptr){
-		std::cerr << "Error: Empty document\n";
-		return EXIT_FAILURE;
-	}
+    //get the first module type
+    nm::TypeManager& typeManager =  **maybeTypeManager;
+    unsigned int first = 0;
+    auto moduleType = typeManager.getUserType(first);
+    if(moduleType==nullptr){
+        std::cerr << "Error: Empty document\n";
+        return EXIT_FAILURE;
+    }
 
-	//get links
-	//TODO support other configurations with multiple inputs and outputs
-	auto inputLink = moduleType->getGraph()->getModule("inputs")->getInput(first);
-	auto outputLink = moduleType->getGraph()->getModule("outputs")->getOutput(first);
+    //get links
+    //TODO support other configurations with multiple inputs and outputs
+    auto inputLink = moduleType->getGraph()->getModule("inputs")->getInput(first);
+    auto outputLink = moduleType->getGraph()->getModule("outputs")->getOutput(first);
 
-	//generate GLSL
-	std::string glslCode = nm::glsl::GlslGenerator::compileToGlslFunction(*inputLink, *outputLink, moduleType->getName());
-	
-	std::cout << glslCode << "\n";
+    //generate GLSL
+    std::string glslCode = nm::glsl::GlslGenerator::compileToGlslFunction(*inputLink, *outputLink, moduleType->getName());
 
-	return EXIT_SUCCESS;
+    std::cout << glslCode << "\n";
+
+    return EXIT_SUCCESS;
 }
